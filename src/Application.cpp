222ac6a11740738b726a98dffed305be7cd07f79
Jônata <Jonatanc0511@gmail.com>
2021-02-15 13:09:07 -0300

    Using vertex buffers

--- src/Application.cpp
+++ src/Application.cpp
@@ -39,7 +39,7 @@ int main(void) {
 	GLFWwindow* window;
 
 	if (!glfwInit())
-        return -1;
+		return -1;
 
 	window = glfwCreateWindow(640, 480, "Polygonnider", NULL, NULL);
 	if (!window) {
@@ -53,6 +53,13 @@ int main(void) {
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
+	unsigned int bufferId;
+	glGenBuffers(6 * sizeof(float), &bufferId);
+	glBindBuffer(GL_ARRAY_BUFFER, bufferId);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), NULL, GL_DYNAMIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (const void *) 0);
+
 	xV1 = yV1 = xV2 = yV2 = 0;
 	xP1 = yP1 = -0.5f;
 	xP2 = yP2 = 0.5f;
@@ -64,12 +71,14 @@ int main(void) {
 		yP1 += yV1;
 		xP2 += xV2;
 		yP2 += yV2;
+		float vertexes[] {
+			xP1, yP1,
+			xP2, yP2,
+			0, 0
+		}; // @TODO don't declare it every frame
 
-		glBegin(GL_TRIANGLES);
-		glVertex2f(xP1, yP1);
-		glVertex2f(xP2, yP2);
-		glVertex2f(0, 0);
-		glEnd();
+		glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), vertexes, GL_DYNAMIC_DRAW);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
 
