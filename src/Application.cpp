142966144deab2dc864f2ffd250d135ac7daa2c5
Jônata <Jonatanc0511@gmail.com>
2021-02-27 10:51:31 -0300

    Parse shaders from files

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,7 +1,42 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 #include "Game.hpp"
+
+static void parseShader(const std::string& filepath, std::string *out_vss, std::string *out_fss) {
+	std::ifstream stream(filepath);
+
+	enum class ShaderType {
+		NONE = -1, VERTEX = 0, FRAGMENT = 1
+	};
+
+	#define getShaderType(line) line.substr(8, 99)
+
+	std::string line;
+	std::stringstream ss[2];
+	ShaderType currType = ShaderType::NONE;
+	while (getline(stream, line)) {
+
+		if (line.substr(0, 7) == "#shader") {
+
+			if (getShaderType(line) == "vertex")
+				currType = ShaderType::VERTEX;
+			else if (getShaderType(line) == "fragment")
+				currType = ShaderType::FRAGMENT;
+
+		} else {
+			ss[(int)currType] << line << '\n';
+		}
+
+	}
+
+	if (out_vss) *out_vss = ss[0].str();
+	if (out_fss) *out_fss = ss[1].str();
+}
 
 static unsigned int compileShader(unsigned int type, const std::string& source) {
 	unsigned int id = glCreateShader(type);
@@ -65,21 +100,13 @@ int main(void) {
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (const void *) 0);
 
-	std::string vsSource = R"glsl(
-		#version 330 core
-		layout(location = 0) in vec4 position;
-		void main() {
-			gl_Position = position;
-		}
-	)glsl";
+	std::string vsSource, fsSource;
+	parseShader("res/shaders/Basic.shader", &vsSource, &fsSource);
+	std::cout << "VERTEX" << std::endl;
+	std::cout << vsSource << std::endl;
+	std::cout << "FRAGMENT" << std::endl;
+	std::cout << fsSource << std::endl;
 
-	std::string fsSource = R"glsl(
-		#version 330 core
-		layout(location = 0) out vec4 color;
-		void main() {
-			color = vec4(1.0, 0.0, 0.0, 1.0);
-		}
-	)glsl";
 	unsigned int program = createShader(vsSource, fsSource);
 	glUseProgram(program);
 
